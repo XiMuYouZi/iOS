35de1b6410aa086ee7b9072fd3f838e7b76f03a8
Mia <476301176@qq.com>
2015-10-22 16:12:35 +0800

    保存、读取应用数据

--- Homepwner/Homepwner/AppDelegate.m
+++ Homepwner/Homepwner/AppDelegate.m
@@ -22,11 +22,13 @@
     ItemsViewController *itemViewController= [[ItemsViewController alloc]init];
     UINavigationController *navController= [[UINavigationController alloc]initWithRootViewController:itemViewController];
     self.window.rootViewController=navController;
+    NSLog(@"%@",NSStringFromSelector(_cmd));
     
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"%@",NSStringFromSelector(_cmd));
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -41,20 +43,24 @@
     }else{
         NSLog(@"Could not save any of BNRItems");
     }
+    NSLog(@"%@",NSStringFromSelector(_cmd));
     
 }
 
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSLog(@"%@",NSStringFromSelector(_cmd));
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"%@",NSStringFromSelector(_cmd));
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"%@",NSStringFromSelector(_cmd));
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
